be8def5933571176e4bd011f3026ff061cd76fa3
raio <rai.conslog@hotmail.com>
2024-12-10 21:08:42 -0300

    added functionality with js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function addTask() {
+    const inputValue = document.getElementById("input").value
+
+    const task = document.getElementById("taskContainer")
+    task.innerHTML += `<div id="contentBox">
+                            <div id="title">
+                                <h4>${inputValue}</h4>
+                            </div>
+                            <div id="buttons">
+                                <button onclick="deleteTask(this)"><ion-icon name="trash-outline"></ion-icon></button>
+                                <input type="checkbox">
+                            </div>
+                        </div>`
+}
+
+function deleteTask(button) {
+    // code goes here
+}
